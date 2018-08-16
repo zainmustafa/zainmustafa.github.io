45a69b775f48483a9dcffe624da045b72bf9ade6
zainmustafa <zmdhillo@gmail.com>
2018-08-16 18:10:57 +0500

    fix strings to tick

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: 'Zain Mustafa',
+    title: `Zain Mustafa`,
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-react-helmet`,
 
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-123676534-1",
+        trackingId: `UA-123676534-1`,
         // Puts tracking script in the head instead of the body
         head: false,
         // Setting this parameter is optional
