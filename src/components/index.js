5520ceea0c06ad62d4c7055eec6d956dea793238
zainmustafa <zmdhillo@gmail.com>
2018-08-16 18:21:04 +0500

    Updates

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import About from './about'
-import Contact from './contact'
-import Header from './header'
-import Main from './main'
-import Project from './project'
-
-export { About, Contact, Header, Main, Project }
