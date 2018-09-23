b46657cd165c0c21c8d83de2efbcae600771c3b1
zainmustafa <zmdhillo@gmail.com>
2018-09-23 15:39:56 +0500

    add bottom scroll to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { About, Contact, Main, Project } from '../components'
-const IndexPage = () => (
-    <div>
+
+
+class IndexPage extends Component {
+  constructor(props) {
+    super(props)
+    this.about = React.createRef();
+    this.main = React.createRef();
+  }
+  componentDidMount() {
+    setTimeout(()=>{
+        this.main.scrollIntoView();
+    },1);
+  }
+
+  render() {
+    return (
+      <div style={{scrollBehavior:"smooth"}}>
         <Contact />
-        <Project />
-        <About />
-        <Main />
-    </div>
-)
+          <Project />
+        <div ref={re => {this.about = re;}} >
+          <About />
+        </div>
+        <div ref={re => {this.main = re}} >
+            <Main />
+        </div>
+      </div>
+    )
+  }
+}
 
 export default IndexPage
